d611475033a5011240309268967426ae90e636b5
leng <leng03@fresnostate.edu>
2017-10-22 22:18:29 -0700

    post working somewhat

--- scrapNrequest.py
+++ scrapNrequest.py
@@ -6,7 +6,7 @@ from time import sleep
 
 source = "https://newsapi.org/v1/articles?source="
 api = "&apiKey=4b6587f8cd2149e9916c4705ad524c3a"
-server = "http://entirenews.tk:3000/api/news?source=bbc"
+server = "http://entirenews.tk:3000/api/news?source=bbc-news"
 SLEEP_TIME_IN_SEC = 1
 SLEEP_TIME_IN_MILI_SEC = 0.3
 
@@ -14,10 +14,10 @@ SLEEP_TIME_IN_MILI_SEC = 0.3
 def send_post_req(url, data1, params=None):
     print('send post req')
     if params is None:
-        params = {'token': 'JNuhg6B7T8jhj8Y68KNKh'}  # params = { 'token': XXX }
+        params = {'token': 'xxx'}  # params = { 'token': XXX }
     else:
-        params['token'] = 'JNuhg6B7T8jhj8Y68KNKh'
-    headers = {'content-type': 'application/json', 'Authorization': 'JNuhg6B7T8jhj8Y68KNKh'}
+        params['token'] = 'xxx'
+    headers = {'content-type': 'application/json', 'Authorization': 'xxx'}
     request = requests.post(url, params=params, data=data1, headers=headers)
     if 200 <= request.status_code < 300:  # Response OK
         print('data posted successfully')
@@ -52,24 +52,28 @@ def scrap_data(url):
     for article in dict_source['articles']:
         #print('scraping:', article['url'])
         dict_url = {}  # dictionary
+        dict_url['source'] = url
         #dict_url['url'] = article['url']  # create dictionary with -key url- and value is the link/url
         dict_url['title'] = article['title']  # add title to dictionary, same for next 3 lines
-        dict_url['text'] = get_text(article['url'])
+        dict_url['article'] = get_text(article['url'])
         dict_url['cover'] = article['urlToImage']
         dict_url['date'] = article['publishedAt']
-        dict_url['source'] = url
-        dict_url['article'] = 'some string'
-        links.append(dict_url)  # combine the all the url from the dictionary created above and store them in a list call links
+        #dict_url['keywords']
+        #dict_url['tags']
+        data = json.dumps(dict_url)
+        send_post_req(server, data)
+        #print(data)
+
+        #links.append(dict_url)  # combine the all the url from the dictionary created above and store them in a list call links
     print('scraping data end')
-    article_dictionary = {}
-    article_dictionary['sources'] = url  # outer dictionary that display where the source of article came from
-    article_dictionary['articles'] = links  # making the list of dictionary created above into articles value
-    data = json.dumps(article_dictionary) # convert dictionary into json string or format
-    print(data)
-    send_post_req(server, data, None)  # post data to server
-
-scrap_data('bbc-news')
+    #article_dictionary = {}
+    #article_dictionary['sources'] = url  # outer dictionary that display where the source of article came from
+    #article_dictionary['articles'] = links  # making the list of dictionary created above into articles value
+    #data = json.dumps(article_dictionary) # convert dictionary into json string or format
 
 
+scrap_data('bild')
+
+# need tags, keywords, url & date
 
 #        if article['publishedAt']:  # only add to dictionary if there is a published date/time
