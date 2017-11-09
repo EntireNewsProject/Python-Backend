4c9634584efccb7db7066f094fc501c7c72201bf
leng03 <leng03@mail.fresnostate.edu>
2017-11-09 15:38:44 -0800

    Formatted Code

--- scrapNrequest.py
+++ scrapNrequest.py
@@ -5,6 +5,7 @@ import json
 from json import loads, dumps
 from time import sleep
 import re
+
 dup_key = []  # contain links of news sources that is already scrap
 # need to automat  the scrap
 
@@ -34,6 +35,7 @@ def send_post_req(url, data1, params=None):
             print('failed to post data, code:', request.status_code)
     print()  # blank line
 
+
 def get_news(url):
     article = Article(url)
     article.download()
@@ -42,19 +44,21 @@ def get_news(url):
     sleep(SLEEP_TIME_IN_MILI_SEC)
     return article
 
+
 def get_text(article):
-    string = r'(Image copyright).+'
-    text = article.text#.replace('\n\n\n','\n\n')
-    matched = re.finditer(string,text)
-    for matchNum,match in enumerate(matched):
-        text = text.replace(match.group(),'')
-    text = text.replace('\n\n\n', '\n\n')
+    reg = r'(Image copyright).+'
+    text = article.text  # .replace('\n\n\n','\n\n')
+    matched = re.finditer(reg, text)
+    for matchNum, match in enumerate(matched):
+        text = text.replace(match.group(), '')
+    text = text.replace('\n\n\n\n', '\n\n')
     text = text.replace('\n\n\n', '\n\n')
     # if url start with bbc
     # then check first \n
     # if it contain
     # replace \n\n\n - \n\n
     return text  # returns the content of url passed in
+
 
 test = 'http://www.bbc.co.uk/news/world-asia-41840866'
 get_text(get_news(test))
@@ -63,7 +67,8 @@ get_text(get_news(test))
 def get_keywords(article):
     return article.keywords
 
-#print(get_keywords('http://www.cnn.com/2017/10/29/politics/angus-king-collusion-calls-sotu/index.html'))
+
+# print(get_keywords('http://www.cnn.com/2017/10/29/politics/angus-king-collusion-calls-sotu/index.html'))
 
 
 def dupicate_checking(key):
@@ -74,10 +79,11 @@ def dupicate_checking(key):
         print('This URL has already been scraped')
         return False
 
+
 def check_article_length(content):
-    if len(content) < 300:
-        print('This text has less than 300 character')
-        print(content)
+    if len(content) < 500:
+        print('This article is less than 500 characters, ignored')
+        # print(content)
         return False
     else:
         return True
@@ -85,8 +91,8 @@ def check_article_length(content):
 
 def scrap_data(url):
     print('scraping data started')
-    req = requests.get(source + url + api)                      # getting articles from source for example: cnn, bbc-news, cnbc, etc
-    dict_source = loads(req.text)                               # reading the content (json format) from source
+    req = requests.get(source + url + api)  # getting articles from source for example: cnn, bbc-news, cnbc, etc
+    dict_source = loads(req.text)  # reading the content (json format) from source
     # if 'error' in req.text:
     #     print('If source is correct try replacing all space with - character')
     #     return
@@ -98,15 +104,15 @@ def scrap_data(url):
                 # print('scraping:', article['url'])
                 dict_url = {}  # dictionary
                 dict_url['source'] = url
-                dict_url['url'] = article['url']                        # create dictionary with -key url- and value is the link/url
-                dict_url['title'] = article['title']                    # add title to dictionary, same for next 3 lines
+                dict_url['url'] = article['url']  # create dictionary with -key url- and value is the link/url
+                dict_url['title'] = article['title']  # add title to dictionary, same for next 3 lines
                 dict_url['article'] = text
                 dict_url['cover'] = article['urlToImage']
                 dict_url['date'] = article['publishedAt']
                 # dict_url['keywords'] = get_keywords(news)
                 # dict_url['tags'] = get_tags(news)
                 data = json.dumps(dict_url)
-                print('test successful')
+                print('scraping successful')
                 send_post_req(server, data)
     print('scraping data end')
 
@@ -131,7 +137,7 @@ def read_array():
             dup_key = file.read().splitlines()
             file.close()
             print('read backup successfully')
-            
+
 
 def main(argv):
     # argv[0]: source
