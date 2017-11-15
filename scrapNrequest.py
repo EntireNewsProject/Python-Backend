149320cebd62035472d8cb381d4682d697b430bd
leng03 <leng03@mail.fresnostate.edu>
2017-11-15 00:31:29 -0800

    add two more functions and add comments

--- scrapNrequest.py
+++ scrapNrequest.py
@@ -7,7 +7,6 @@ from time import sleep
 import re
 
 dup_key = []  # contain links of news sources that is already scrap
-# need to automat  the scrap
 
 source = "https://newsapi.org/v1/articles?source="
 api = "&apiKey=4b6587f8cd2149e9916c4705ad524c3a"
@@ -15,17 +14,16 @@ server = "http://entirenews.tk:3000/api/news?source=bbc-news"
 SLEEP_TIME_IN_SEC = 1
 SLEEP_TIME_IN_MILI_SEC = 0.3
 
-
 # token is needed
 def send_post_req(url, data1, params=None):
     print('send post req')
     if params is None:
-        params = {'token': 'xxx'}  # params = { 'token': XXX }
+        params = {'token': 'xxx'}                # params = { 'token': XXX }
     else:
         params['token'] = 'xxx'
     headers = {'content-type': 'application/json', 'Authorization': 'xxx'}
     request = requests.post(url, params=params, data=data1, headers=headers)
-    if 200 <= request.status_code < 300:  # Response OK
+    if 200 <= request.status_code < 300:         # Response OK
         print('data posted successfully')
     else:
         json = loads(request.text)
@@ -36,7 +34,7 @@ def send_post_req(url, data1, params=None):
     print()  # blank line
 
 
-def get_news(url):
+def get_news(url):                               # Scraping of articles from provided source
     article = Article(url)
     article.download()
     sleep(SLEEP_TIME_IN_MILI_SEC)
@@ -45,45 +43,44 @@ def get_news(url):
     return article
 
 
+def replace_extra_line(texts):
+    texts = texts.replace('\n\n\n\n', '\n\n')    # replacing extra lines with just double line spacing
+    texts = texts.replace('\n\n\n', '\n\n')
+    return texts
+
+
 def get_text(article):
+    text = replace_extra_line(article.text)
+    text = text.replace('Media playback is unsupported on your device Media caption ','')
+    return text
+
+
+def replace_text(article):                  # function replace the sentence that start with Image copyright with blank
     reg = r'(Image copyright).+'
-    text = article.text  # .replace('\n\n\n','\n\n')
+    text = article
     matched = re.finditer(reg, text)
     for matchNum, match in enumerate(matched):
         text = text.replace(match.group(), '')
-    text = text.replace('\n\n\n\n', '\n\n')
-    text = text.replace('\n\n\n', '\n\n')
-    # if url start with bbc
-    # then check first \n
-    # if it contain
-    # replace \n\n\n - \n\n
-    return text  # returns the content of url passed in
-
-
-test = 'http://www.bbc.co.uk/news/world-asia-41840866'
-get_text(get_news(test))
+    text = replace_extra_line(text)
+    return text
 
 
 def get_keywords(article):
     return article.keywords
 
 
-# print(get_keywords('http://www.cnn.com/2017/10/29/politics/angus-king-collusion-calls-sotu/index.html'))
-
-
 def dupicate_checking(key):
     if key not in dup_key:
-        dup_key.append(key)
+        dup_key.append(key)                 # Save url into dup_key list for storing later
         return True
     else:
         print('This URL has already been scraped')
         return False
 
 
-def check_article_length(content):
+def check_article_length(content):          # Only post articles that have more than 500 character in them
     if len(content) < 500:
         print('This article is less than 500 characters, ignored')
-        # print(content)
         return False
     else:
         return True
@@ -91,33 +88,35 @@ def check_article_length(content):
 
 def scrap_data(url):
     print('scraping data started')
-    req = requests.get(source + url + api)  # getting articles from source for example: cnn, bbc-news, cnbc, etc
-    dict_source = loads(req.text)  # reading the content (json format) from source
-    # if 'error' in req.text:
-    #     print('If source is correct try replacing all space with - character')
-    #     return
+    req = requests.get(source + url + api)      # getting articles from source for example: cnn, bbc-news, cnbc, etc
+    dict_source = loads(req.text)               # reading the content (json format) from source
+    if req.status_code == 400:                  # If unable to find source then exit
+        print('Source is incorrect or try replacing all spaces with " - " character')
+        return
     for article in dict_source['articles']:
         if dupicate_checking(article['url']):
             news = get_news(article['url'])
             text = get_text(news)
+            if url == 'bbc-news':
+                text = replace_text(text)
             if check_article_length(text):
                 # print('scraping:', article['url'])
                 dict_url = {}  # dictionary
-                dict_url['source'] = url
-                dict_url['url'] = article['url']  # create dictionary with -key url- and value is the link/url
-                dict_url['title'] = article['title']  # add title to dictionary, same for next 3 lines
+                dict_url['source'] = url                        # name of source where articles are getting scraped from
+                dict_url['url'] = article['url']                # add ['name'] to dictionary, same for next 4 lines
+                dict_url['title'] = article['title']
                 dict_url['article'] = text
                 dict_url['cover'] = article['urlToImage']
                 dict_url['date'] = article['publishedAt']
                 # dict_url['keywords'] = get_keywords(news)
                 # dict_url['tags'] = get_tags(news)
                 data = json.dumps(dict_url)
-                print('scraping successful')
                 send_post_req(server, data)
+                print('Posting article done')
     print('scraping data end')
 
 
-def save_array():
+def save_array():                               # save url string
     global dup_key
     file = open('dup_key.db', 'w')
     for links in dup_key:
@@ -126,7 +125,7 @@ def save_array():
     print('backup done')
 
 
-def read_array():
+def read_array():                               # read stored url string
     global dup_key
     try:
         file = open('dup_key.db', 'r')
@@ -140,12 +139,10 @@ def read_array():
 
 
 def main(argv):
-    # argv[0]: source
     if len(argv) == 1:
         read_array()
-        scrap_data(argv[0])
+        scrap_data(argv[0])                     # argv[0]: source
         save_array()
-        # exit
     else:
         print('RuntimeError: incorrect number of args')
         sys.exit()
