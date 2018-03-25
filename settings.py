0be2fbad826e17e5b8b1405b65f263fd68521833
khushaljobanputra <khushal2611@gmail.com>
2018-03-24 22:34:46 -0700

    fixed path for stop word file

--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@ import os
 
 PARENT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
-NLP_STOPWORDS_EN = os.path.join(PARENT_DIRECTORY, 'resources/misc/stopwords-nlp-en.txt')
+NLP_STOPWORDS_EN = os.path.join(PARENT_DIRECTORY, 'resources/stopwords-nlp-en.txt')
