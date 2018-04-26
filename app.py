1884cd8b9b285f0af98b6edd3684d6478768640a
khushaljobanputra <khushal2611@gmail.com>
2018-04-26 13:16:22 -0700

    testing

--- app.py
+++ app.py
@@ -224,9 +224,10 @@ def job():
     save_array()
 
 
-schedule.every(20).minutes.do(job)
+# schedule.every(20).minutes.do(job)
 
 if __name__ == '__main__':
-    while 1:
-        schedule.run_pending()
-        time.sleep(1)
+    # while 1:
+    #     schedule.run_pending()
+    #     time.sleep(1)
+    job()
