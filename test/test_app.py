feaba8b82479f1dfcabe651f3d6cc404556f3199
kishanshah <kishanshah147@gmail.com>
2018-05-01 11:44:44 -0700

    Optimizing imports

--- test/test_app.py
+++ test/test_app.py
@@ -1,18 +1,14 @@
 """
 Entire News - Python Scrapper and NLP unit tests.
 """
-import sys
 import os
+import sys
+import time
+import traceback
 import unittest
-
-
-
-# from collections import defaultdict, OrderedDict
-# import concurrent.futures
 
 TEST_DIR = os.path.abspath(os.path.dirname(__file__))
 PARENT_DIR = os.path.join(TEST_DIR, '..')
-
 
 sys.path.insert(0, PARENT_DIR)
 
@@ -20,11 +16,8 @@ TEXT_FN = os.path.join(TEST_DIR, 'data', 'text')
 HTML_FN = os.path.join(TEST_DIR, 'data', 'html')
 URLS_FILE = os.path.join(TEST_DIR, 'data', 'fulltext_url_list.txt')
 
-# import newspaper
-# from newspaper import app, nlp
-# from newspaper import Article, fulltext, Source, ArticleException, news_pool
-# from newspaper.configuration import Configuration
-# from newspaper.urls import get_domain
+from newspaper import Article
+from newspaper.urls import get_domain
 
 
 def print_test(method):
